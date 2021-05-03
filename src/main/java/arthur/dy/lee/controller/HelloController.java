0e8845ba8578662e478b2255a51f427417a9c462
arthurlee <paincupid@hotmail.com>
2021-05-03 23:16:12 +0800

    1

---
+++ src/main/java/arthur/dy/lee/controller/HelloController.java
@@ -0,0 +1,13 @@
+package arthur.dy.lee.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World2";
+    }
+}
